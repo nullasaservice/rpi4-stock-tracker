215e1ccb2df5c050deaa71c47d6fa926f1066b37
nullasaservice <carlos98mlg@gmail.com>
2022-06-11 10:32:00 +0200

    allow multiple SKU search

--- src/index.rb
+++ src/index.rb
@@ -13,17 +13,21 @@ rejected_regions = ENV['REJECTED_REGIONS']
 rejected_regions = rejected_regions.split ","
 rejected_regions.map! { |r| "(" + r + ")" }
 
+# initialize rpi_sku_array
+rpi_sku_array = ENV['RPI_SKU_ARRAY']
+rpi_sku_array = rpi_sku_array.split ","
+
 log_app "App running! Running cron job each #{ENV['SCHEDULER_TIME_FREQUENCY']}."
 
 scheduler.every ENV['SCHEDULER_TIME_FREQUENCY'] do
   log_cron "Starting task..."
   rpi_locator_entries = RpiLocatorSpider.parse!(:parse, url: "https://rpilocator.com")
-  rpi4_8gb_entries_eur = rpi_locator_entries.select { |e| e.sku == ENV['RPI_SKU'] && rejected_regions.none? { |zone| e.vendor.include? zone } }
-  if rpi4_8gb_entries_eur.empty?
-    log_app "No #{ENV['RPI_SKU']} in stock in Europe."
+  wanted_rpi_stock_entries = rpi_locator_entries.select { |e| rpi_sku_array.any? { |sku| sku == e.sku } && rejected_regions.none? { |zone| e.vendor.include? zone } }
+  if wanted_rpi_stock_entries.empty?
+    log_app "No #{ENV['RPI_SKU']} in stock with given restrictions."
   else
     log_app "Stock found!"
-    send_tg_notification_stock rpi4_8gb_entries_eur
+    send_tg_notification_stock wanted_rpi_stock_entries
   end
 end
 
